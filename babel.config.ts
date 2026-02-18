16cf43d9959c00ad462d0456fce76183d9d0ab67
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2026-02-18 23:18:19 +0530

    chore: setup uniwind

--- babel.config.ts
+++ babel.config.ts
@@ -1,9 +1,6 @@
 module.exports = function (api: any) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
